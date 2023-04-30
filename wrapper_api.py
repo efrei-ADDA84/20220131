8f6c8a1cd3f8471f9f692f583b79462b9e9e92b0
kymsy <kymsyguzman@gmail.com>
2023-04-30 21:44:03 +0200

    modification wrapper_api

--- wrapper_api.py
+++ wrapper_api.py
@@ -1,8 +1,6 @@
 import os
 import requests
 from fastapi import FastAPI
-from pydantic import BaseModel
-
 
 app = FastAPI()
 
@@ -10,24 +8,30 @@ API_Key = os.environ.get('API_KEY')
 lat= os.environ.get("LAT")
 lon = os.environ.get("LONG")
 
-# Vérification des variables d'environnement
-if API_Key is None:
-    raise ValueError("La variable d'environnement OPENWEATHER_API_KEY est manquante")
-if lat is None:
-    raise ValueError("La variable d'environnement LAT est manquante")
-if lon is None:
-    raise ValueError("La variable d'environnement LONG est manquante")
-
+if 'API_KEY' not in os.environ:
+    print("API_KEY environment variable not defined.")
+    exit(1)
+if 'LAT' not in os.environ:
+    print("LAT environment variable not defined.")
+    exit(1)
+if 'LONG' not in os.environ:
+    print("LONG environment variable not defined.")
+    exit(1)
 
 @app.get("/")
-def get_weather():
+async def root():
+    return {"message": "Welcome to my weather API!"}
+
+def get_weather(lat, long):     
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_Key}"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
         weather = data['weather'][0]['description']
         temperature = data['main']['temp'] - 273.15 # Convert Kelvin to Celsius
-        return {'weather': weather, 'temperature': temperature}
+        return f'The weather in your location is {weather} with a temperature of {temperature:.2f}°C'
     else:
-        return {'error': 'Failed to retrieve weather data'}
+        return 'Failed to retrieve weather data'
 
+
+print(get_weather(lat, lon))
