5525682b41336797792cf84d0538caaec534254e
kymsy <kymsyguzman@gmail.com>
2023-04-30 21:47:14 +0200

    modification wrapper_api

--- wrapper_api.py
+++ wrapper_api.py
@@ -4,25 +4,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+
 API_Key = os.environ.get('API_KEY')
 lat= os.environ.get("LAT")
 lon = os.environ.get("LONG")
 
-if 'API_KEY' not in os.environ:
-    print("API_KEY environment variable not defined.")
-    exit(1)
-if 'LAT' not in os.environ:
-    print("LAT environment variable not defined.")
-    exit(1)
-if 'LONG' not in os.environ:
-    print("LONG environment variable not defined.")
-    exit(1)
-
-@app.get("/")
-async def root():
-    return {"message": "Welcome to my weather API!"}
-
-def get_weather(lat, long):     
+def get_weather(lat, long):
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_Key}"
     response = requests.get(url)
     if response.status_code == 200:
