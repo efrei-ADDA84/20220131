854a0c69e752d8b746f43a431a2890d55473e23b
kymsy <kymsyguzman@gmail.com>
2023-04-30 22:55:27 +0200

    modification wrapper_api

--- wrapper_api.py
+++ wrapper_api.py
@@ -5,21 +5,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
+async def root(lat: float, lon: float):
+    API_Key = os.environ.get('API_KEY')
 
-API_Key = os.environ.get('API_KEY')
-lat= os.environ.get("LAT")
-lon = os.environ.get("LONG")
+    def get_weather(lat, lon):
+        url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_Key}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            weather = data['weather'][0]['description']
+            temperature = data['main']['temp'] - 273.15 # Convert Kelvin to Celsius
+            return f'The weather in your location is {weather} with a temperature of {temperature:.2f}°C'
+        else:
+            return 'Failed to retrieve weather data'
 
-def get_weather(lat, long):
-    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_Key}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        weather = data['weather'][0]['description']
-        temperature = data['main']['temp'] - 273.15 # Convert Kelvin to Celsius
-        return f'The weather in your location is {weather} with a temperature of {temperature:.2f}°C'
-    else:
-        return 'Failed to retrieve weather data'
-
-
-print(get_weather(lat, lon))
+    return get_weather(lat, lon)
